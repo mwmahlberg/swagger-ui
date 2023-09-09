3f16d1765f596909c91c68d6c45136e8ad585711
Markus Mahlberg <markus.mahlberg@dacoso.com>
2023-09-09 16:07:59 +0200

    Revert "Add 'swagger-ui/' from commit 'ea6e3985871cdd2e949d8b890f4681f6920dda0f'"
    This reverts commit 66c224158cb5250af1154b1ccab188f55d62acf4.

--- swagger-ui/src/standalone/plugins/top-bar/components/Logo.jsx
+++
@@ -1,9 +0,0 @@
-/**
- * @prettier
- */
-import React from "react"
-import SwaggerUILogo from "../assets/logo_small.svg"
-
-const Logo = () => <img height="40" src={SwaggerUILogo} alt="Swagger UI" />
-
-export default Logo
