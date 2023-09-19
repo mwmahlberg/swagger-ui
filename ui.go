6b06db688f7520738a0658d999da368297d47b4f
Markus Mahlberg <markus@mahlberg.io>
2023-09-19 09:44:08 +0200

    Remove custom Errors which are superseeded by validation errors.

--- ui.go
+++ ui.go
@@ -1,3 +1,21 @@
+/*
+ *  ui.go is part of github.com/mwmahlberg/swagger-ui project.
+ *
+ *  Copyright 2023 Markus W Mahlberg
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
 package swaggerui
 
 import (
@@ -20,13 +38,6 @@ type HandlerOption func(*SwaggerUi)
 const (
 	DefaultSpecfileName = "swagger.yaml"
 	InitializerFilename = "swagger-initializer.js"
-)
-
-var (
-	ErrEmptyData     = errors.New("no data was provided for the spec file")
-	ErrInvalidJson   = errors.New("input is not valid JSON data")
-	ErrEmptyFileName = errors.New("empty file name was provided")
-	ErrNoJsonSuffix  = errors.New("name JSON spec does not end in '.json'")
 )
 
 // SwaggerUi is a handler that serves the swagger-ui.
